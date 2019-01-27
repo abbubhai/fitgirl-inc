91685682d01d54b6584f132e449f0d1a869a22f0
Joe Lofshult <jlofshult@unomaha.edu>
2019-01-26 19:47:29 -0600

    Working on logic to pull days from physical pages

--- update_program_dates.py
+++ update_program_dates.py
@@ -2,14 +2,12 @@ import psycopg2
 import re
 from datetime import date, timedelta
 
-today = date.today()
-print(today)
-
+#new_start_date = input("New start date (yyyy/mm/dd): ").split('/')
+new_start_date = ['2019', '11', '11']
+program = 'Fall 2019'
+new_start_date = date(int(new_start_date[0]), int(new_start_date[1]), int(new_start_date[2]))
 one_week = timedelta(days=7)
 one_day = timedelta(days=1)
-print(today + one_week)
-print(today + one_day)
-
 
 # Connect to an existing database
 conn = psycopg2.connect("dbname=empoweru3 user=postgres password=instructor1a")
@@ -21,7 +19,7 @@ cur = conn.cursor()
 myProgram = cur.execute("""
 select path from wagtailcore_page
 where title like %s;
-""", ('Spring 2019', ))
+""", (program, ))
 
 program_page_id = cur.fetchone()
 program_page_id = "%"+program_page_id[0]+"%"
@@ -29,13 +27,28 @@ program_page_id = "%"+program_page_id[0]+"%"
 myWeeks = cur.execute("""
 select title, id, path from wagtailcore_page
 where path like %s and title not like %s
-""", (program_page_id, "Spring 2019", ))
+""", (program_page_id, program, ))
 
 for result in cur.fetchmany(500):
     m = re.search(('Week.*'), result[0])
     if (m):
         week_name = m.group(0)
         week_id = result[1]
+        week_path = result[2] + '%'
+        print(week_path)
+        myPhys = cur.execute("""
+                select id from wagtailcore_page
+                where path like %s;
+                """, (week_path,))
+        result = cur.fetchmany()
+        print(result)
+
+        myPhysDates = cur.execute("""
+                select start_date, end_date
+                from week_physicalpostpage
+                where page_ptr_id = %s""", (result[0],))
+        result = cur.fetchmany()
+
         myWeek = cur.execute("""
         select start_date, end_date from week_weekpage
         where page_ptr_id = %s
@@ -44,6 +57,7 @@ for result in cur.fetchmany(500):
         TIME_FORMAT = "%b %d"
         print("Start date for %s: %s" %(week_name, result[0].strftime(TIME_FORMAT)))
         print("End date for %s: %s" %(week_name, result[1].strftime(TIME_FORMAT)))
+
 
 cur.close()
 conn.close()
